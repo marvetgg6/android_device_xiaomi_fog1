628934df0b4427f1af62b06e71e1dcc5c8612876
Qukeerr <58364006+marvetgg6@users.noreply.github.com>
2023-06-23 13:04:14 +0000

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_spes.mk
+    $(LOCAL_DIR)/havoc_fog.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_spes-user \
-    lineage_spes-userdebug \
-    lineage_spes-eng
+    havoc_fog-user \
+    havoc_fog-userdebug \
+    havoc_fog-eng
